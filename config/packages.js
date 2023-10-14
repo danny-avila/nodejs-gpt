28082c7cf5e808eec6c125dbcb64e0fc236a24de
Danny Avila <messagedaniel@protonmail.com>
2023-10-14 09:17:46 -0400

    dev: husky eslint commit

--- config/packages.js
+++ config/packages.js
@@ -5,27 +5,27 @@ import fs from 'fs';
 import { deleteNodeModules } from './helpers.js';
 
 // Set the directories
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const rootDir = path.resolve(__dirname, '..');
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const rootDir = path.resolve(dirname, '..');
 const directories = [rootDir];
 
-// Delete package-lock.json if it exists
+// Delete package-lock if it exists
 const packageLockPath = path.resolve(rootDir, 'package-lock.json');
 if (fs.existsSync(packageLockPath)) {
-  console.purple('Deleting package-lock.json...');
-  fs.unlinkSync(packageLockPath);
+    console.purple('Deleting package-lock.json...');
+    fs.unlinkSync(packageLockPath);
 }
 
 (async () => {
-  // Delete all node_modules
-  directories.forEach(deleteNodeModules);
+    // Delete all node_modules
+    directories.forEach(deleteNodeModules);
 
-  // Run npm cache clean --force
-  console.purple('Cleaning npm cache...');
-  execSync('npm cache clean --force', { stdio: 'inherit' });
+    // Run npm cache clean --force
+    console.purple('Cleaning npm cache...');
+    execSync('npm cache clean --force', { stdio: 'inherit' });
 
-  // Install dependencies
-  console.purple('Installing dependencies...');
-  execSync('npm install', { stdio: 'inherit' });
+    // Install dependencies
+    console.purple('Installing dependencies...');
+    execSync('npm install', { stdio: 'inherit' });
 })();
